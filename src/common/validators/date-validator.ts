683b70334ce75343cee6c0044334bbce90f2d6f4
sxlwrl <vadymkazak2019@gmail.com>
2025-06-14 21:29:53 +0300

    [add] custom validator for date in dto

---
+++ src/common/validators/date-validator.ts
@@ -0,0 +1,22 @@
+import { registerDecorator, ValidationOptions, ValidationArguments } from 'class-validator';
+
+export function IsValidDate(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'isValidDate',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate(value: any) {
+          if (!value) return false;
+          const date = new Date(value);
+          return date <= new Date();
+        },
+        defaultMessage(args: ValidationArguments) {
+          return `${args.property} cannot be in the future`;
+        },
+      },
+    });
+  };
+} 
