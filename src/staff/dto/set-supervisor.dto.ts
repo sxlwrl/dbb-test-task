ee3725c89f8df2ab342cb431e726c1222f7d2d07
sxlwrl <vadymkazak2019@gmail.com>
2025-06-14 21:31:47 +0300

    [feat] swagger information

--- src/staff/dto/set-supervisor.dto.ts
+++ src/staff/dto/set-supervisor.dto.ts
@@ -1,6 +1,8 @@
 import { IsUUID, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SetSupervisorDto {
+  @ApiProperty({ example: '48fceb7b-5d33-4d9c-af50-a683eb90b42b', description: 'Supervisor ID' })
   @IsUUID()
   @IsOptional()
   supervisorId?: string | null;
